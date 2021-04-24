3d40b526c21aa0d98d90ce8b9d799d47f45edffc
Alexandr Siryj <theobroma222@gmail.com>
2021-04-24 20:46:33 +0300

    add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>QuizApp</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>123</main>
+      <footer></footer>
+    </div>
+  );
+}
