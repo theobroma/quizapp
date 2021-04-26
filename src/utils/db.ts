28348dc9f9a7d5272f080150285cf51d7b0151c2
Alexandr Siryj <theobroma222@gmail.com>
2021-04-26 11:10:44 +0300

    quiz form

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -8,3 +8,8 @@ export const addUser = async (authUser: any) => {
     .set({ ...authUser }, { merge: true });
   return resp;
 };
+
+export const addQuiz = async (quizData) => {
+    let response = await firebase.firestore().collection('quiz').add(quizData);
+    return response;
+  };
